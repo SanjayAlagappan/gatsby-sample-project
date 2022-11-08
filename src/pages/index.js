eade82b62bf12d266d2007d0d26f980da6f89659
Sanjay A <96683733+SanjayAlagappan@users.noreply.github.com>
2022-11-08 13:51:09 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,7 @@ export default function Homepage(props) {
         const Component = sections[blocktype] || Fallback
         return <Component key={id} {...componentProps} />
       })}
+      <h1>This is sample HTML header</h1>
     </Layout>
   )
 }
